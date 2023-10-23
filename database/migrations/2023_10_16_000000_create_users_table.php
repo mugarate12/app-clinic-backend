9cdb0ac86582c7a937a82e516f747c3666a2291f
Mateus Cardoso <serjumano17@gmail.com>
2023-10-23 17:17:39 -0300

    build: :card_file_box: add migrations to create initial tables user_groups and users

--- database/migrations/2023_10_16_000000_create_users_table.php
+++ database/migrations/2023_10_16_000000_create_users_table.php
@@ -12,13 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->uuid('user_group_id_FK')->index();
+
+            $table->foreign('user_group_id_FK')
+                ->references('id')
+                ->on('users_groups');
         });
     }
 
