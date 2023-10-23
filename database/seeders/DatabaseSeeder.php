a978bc508bebc32a73850b7d364396e7bc640b0a
Mateus Cardoso <serjumano17@gmail.com>
2023-10-23 17:19:39 -0300

    feat: :card_file_box: add seed to create user groups and initial user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Factories\UsersGroupsFactory;
+use App\Enums\UsersGroupsEnum;
+use App\Models\UsersGroup;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $uses_groups = UsersGroupsEnum::getValues();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($uses_groups as $value) {
+            $user_group_in_database = UsersGroup::where('name', $value)->first();
+
+            if (is_null($user_group_in_database)) {
+                UsersGroupsFactory::new()->create([
+                    'name' => $value,
+                ]);
+            };
+        };
+
+        // create first admin user
+        $administrator_user_group_in_database = UsersGroup::where('name', UsersGroupsEnum::Admin)->first();
+
+        // get all users
+        $users = User::all();
+
+        if ($users->count() == 0) {
+            User::factory()->create([
+                'name' => 'admin',
+                'email' => 'mail@mail.com',
+                'password' => bcrypt('admin'),
+                'user_group_id_FK' => $administrator_user_group_in_database->id,
+            ]);
+        }
+
     }
 }
